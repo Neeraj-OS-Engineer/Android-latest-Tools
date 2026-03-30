de5cd51909ceee1c5cba4f1719319ae35160d4b9
Neeraj Engineer <haickarpin2998@gmail.com>
2026-03-30 10:44:32 +0530

    Create MediaItem.java

---
+++ app/main/java/com/ns/dev/jdkhandlookingdeep/MediaItem.java
@@ -0,0 +1,20 @@
+package com.ns.dev.jdkhandlookingdeep;
+
+public class MediaItem {
+    private String title;
+    private String path;
+    private String mimeType;
+    private long duration;
+    private long size;
+
+    // Constructor, getters, setters
+    public MediaItem(String title, String path, String mimeType, long duration, long size) {
+        this.title = title;
+        this.path = path;
+        this.mimeType = mimeType;
+        this.duration = duration;
+        this.size = size;
+    }
+
+    // Getters and setters...
+}
